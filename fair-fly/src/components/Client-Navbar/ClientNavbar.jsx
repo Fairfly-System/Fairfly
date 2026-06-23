fe98c0903cb9851b70fd0d201f083f7ce24e3c31
Rei Seyer <reimierreyes0221@gmail.com>
2026-06-23 16:42:09 +0800

    Updated organization files.

--- fair-fly/src/components/Client-Navbar/ClientNavbar.jsx
+++ fair-fly/src/components/Client-Navbar/ClientNavbar.jsx
@@ -1,37 +1,49 @@
-import React from 'react'
-import './client-navbar.css'
-import { NavLink } from 'react-router'
+import React from 'react';
+import './client-navbar.css';
+import { NavLink, useNavigate } from 'react-router';
+import { signOut } from 'firebase/auth';
+import { auth } from '../../firebase';
+
 
 export default function ClientNavbar({ onNavigate, currentPage }) {
+  const navigate = useNavigate();
+
+  const handleLogout = async (e) => {
+    e.preventDefault();
+
+    try {
+      await signOut(auth);
+      console.log('User signed out');
+      navigate('/');
+    } catch (error) {
+      console.error('Logout error:', error);
+    }
+  };
+
   return (
     <nav className="client-nav">
-        <NavLink to="/home"
-        >
+      <NavLink to="/home">
         <div className="client-logoIcon">
-
-        <img src='FairflyLogo.png'></img>
-
+          <img src="/FairflyLogo.png" alt="Fairfly Logo" />
         </div>
+      </NavLink>
 
-        </NavLink>
+      <div className="client-navRight">
+        <span className="client-welcome">
+          Welcome, client@email.com
+        </span>
 
-        <div className="client-navRight">
-          <span className="client-welcome">
-            Welcome, client@email.com
-          </span>
-
-          <a
-            href="#"
-            className="client-logout"
-            onClick={(e) => {
-              e.preventDefault()
-              onNavigate && onNavigate('home')
-            }}
-          >
-            <p><i class="fa-solid fa-arrow-right-from-bracket"></i></p>
-            Logout
-          </a>
-        </div>
-      </nav>
-  )
+        <a
+          href="#"
+          className="client-logout"
+          onClick={handleLogout}
+        >
+          <p>
+            <i className="fa-solid fa-arrow-right-from-bracket"></i>
+          </p>
+          Logout
+        </a>
+      </div>
+    </nav>
+  );
 }
